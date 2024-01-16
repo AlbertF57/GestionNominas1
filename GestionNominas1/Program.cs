d2700276ab512ffecefbb513b75cacde0708a843
DAM 1 <DAM 1@DESKTOP-4A99GNK>
2024-01-16 09:53:19 +0100

    Agregar archivos de proyecto.

---
+++ GestionNominas1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionNominas1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
